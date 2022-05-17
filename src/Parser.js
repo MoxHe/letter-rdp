5ac38223452a00aef442e8c375047bd168efa418
Moxiao He <moxiao_he@intuit.com>
2022-05-16 18:47:32 -0700

    add stringLiteral

--- src/Parser.js
+++ src/Parser.js
@@ -1,12 +1,29 @@
+const { Tokenizer } = require('./Toeknizer');
+
+
 /**
  * Letter Parser: recursive descent implementation.
  */
 class Parser {
   /**
+   * Initializes the parser.
+   */
+  constructor() {
+    this._string = '';
+    this._tokenizer = new Tokenizer();
+  }
+
+  /**
    * Parses a string into an AST.
    */
   parse(string) {
     this._string = string;
+    this._tokenizer.init(string);
+
+    // Prime the tokenizer to obtain the first
+    // toekn which is our lookahead. The lookahead is
+    // used for predictive parsing.
+    this._lookahead = this._tokenizer.getNextToken();
 
     // Parse recursively starting form the main
     // entry point, the Program:
@@ -18,13 +35,43 @@ class Parser {
    * Main entry point.
    *
    * Program
-   *  : NumericLiteral
+   *  : Literal
    *  ;
    */
   Program() {
     return {
       type: 'Program',
-      body: this.NumericLiteral(),
+      body: this.Literal(),
+    };
+  }
+
+  /**
+   * Literal
+   *   : NumericLiteral
+   *   | StringLiteral
+   *   ;
+   */
+  Literal() {
+    switch(this._lookahead.type) {
+      case 'NUMBER':
+        return this.NumericLiteral();
+      case 'STRING':
+        return this.StringLiteral();
+    }
+
+    throw new SyntaxError(`Literal: unexpected literal production`);
+  }
+
+  /**
+   * StringLiteral
+   *   : STRING
+   *   ;
+   */
+  StringLiteral() {
+    const token = this._eat('STRING');
+    return {
+      type: 'StringLiteral',
+      value: token.value.slice(1, -1),
     };
   }
 
@@ -34,10 +81,35 @@ class Parser {
    *  ;
    */
   NumericLiteral() {
+    const token = this._eat('NUMBER');
     return {
       type: 'NumericLiteral',
-      value: Number(this._string),
+      value: Number(token.value),
+    };
+  }
+
+  /**
+   * Expects a token of a given type.
+   */
+  _eat(tokenType) {
+    const token = this._lookahead;
+
+    if (token === null) {
+      throw new SyntaxError(
+        `Unexpected end of input, expected: "${tokenType}"`
+      )
     }
+
+    if (token.type !== tokenType) {
+      throw new SyntaxError(
+        `Unexpected token: "${token.value}", expected: "${tokenType}"`
+      )
+    }
+
+    // Advance to next token.
+    this._lookahead = this._tokenizer.getNextToken();
+
+    return token;
   }
 }
 
